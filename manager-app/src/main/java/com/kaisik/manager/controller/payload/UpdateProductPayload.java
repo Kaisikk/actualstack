230935ca83019dc3377745d26d4c1dc7df902f61
Kaisikk <Kosikov.Ilya@mail.ru>
2024-09-02 20:53:32 +0400

    Добавлена обработка ошибок

--- manager-app/src/main/java/com/kaisik/manager/controller/payload/UpdateProductPayload.java
+++ manager-app/src/main/java/com/kaisik/manager/controller/payload/UpdateProductPayload.java
@@ -5,9 +5,9 @@ import jakarta.validation.constraints.Size;
 
 public record UpdateProductPayload(
 
-        @NotNull
-        @Size(min = 3, max = 50)
+        @NotNull(message = "{catalogue.products.update.errors.title_is_null}")
+        @Size(min = 3, max = 50, message = "{catalogue.products.update.errors.title_size_is_invalid}")
         String title,
-        @Size(max = 1000)
+        @Size(max = 1000, message = "{catalogue.products.update.errors.details_size_is_invalid}")
         String details) {
 }
