3046217d15882c244e503c7152f138b1c5a604b0
Kaisikk <Kosikov.Ilya@mail.ru>
2024-09-06 22:30:34 +0400

    Создание Сервиса для обмена данных между модулями

---
+++ manager-app/src/main/java/com/kaisik/client/RestClientProductRestClient.java
@@ -0,0 +1,48 @@
+package com.kaisik.client;
+
+import com.kaisik.entity.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class RestClientProductRestClient implements ProductRestClient {
+
+    private static final ParameterizedTypeReference<List<Product>> PRODUCTS_TYPE_REFERENCE =
+            new ParameterizedTypeReference<List<Product>>() {
+            };
+
+    private final RestClient restClient;
+
+    @Override
+    public List<Product> findAllProducts() {
+        return this.restClient
+                .get()
+                .uri("/catalogue-api/products/")
+                .retrieve()
+                .body(PRODUCTS_TYPE_REFERENCE);
+    }
+
+    @Override
+    public Product createProduct(String title, String details) {
+        return null;
+    }
+
+    @Override
+    public Optional<Product> findProduct(int productId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void updateProduct(int product, String title, String details) {
+
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+
+    }
+}
